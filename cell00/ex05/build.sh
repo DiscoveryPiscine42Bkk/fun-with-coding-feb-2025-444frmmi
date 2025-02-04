57d7d5506a5c3c2c6d5b51f77290208d4919bf0a
444frmmi <nattaratfifremmi@gmail.com>
2025-02-04 23:04:08 +0700

    ex05

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,10 +1,3 @@
-if [ $# -eq 0 ]; then
-    echo "Usage: ./build.sh <folder_name1> <folder_name2> ..."
-    exit 1
-fi
+[ $# -eq 0 ] && echo "No arguments supplied" && exit 1
 
-for folder in "$@"
-do
-    mkdir -p "ex$folder"
-    echo "Create folder: ex$folder"
-done
+for arg in "$@"; do mkdir "ex$arg"; done
